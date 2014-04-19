31fe7a60f3a0acbde87abe56fedc114d9906f228
Jing Sheng <442730562@qq.com>
2014-04-19 20:20:08 +0800

    添加收藏

---
+++ php/test_user_collect.php
@@ -0,0 +1,16 @@
+<?php
+	
+	require_once 'include/User_Collect.php';
+
+	$test = new User_Collect();
+	$response = array("success" => 0, "error" => 0);
+	$collect = $test->delete_collected_cars(1, 12);
+	if($collect){
+		$response['success']=1;
+		$response['collect_time']=$collect['collect_time'];
+		echo json_encode($response);
+	}else{
+		$response['error']=0;
+		echo json_encode($response);
+	}
+?>
