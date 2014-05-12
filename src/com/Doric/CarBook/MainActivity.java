c156587ff8bd4bacbbb4ba8cb13fe3f6c9579131
Vincentbel <buaazqh@gmail.com>
2014-05-12 22:15:13 +0800

    主界面修改

--- src/com/Doric/CarBook/MainActivity.java
+++ src/com/Doric/CarBook/MainActivity.java
@@ -1,10 +1,21 @@
 package com.Doric.CarBook;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import com.Doric.CarBook.car.CarShow;
 import com.Doric.CarBook.member.Login;
 import com.Doric.CarBook.member.PersonalCenter;
@@ -13,12 +24,55 @@ import com.Doric.CarBook.search.AlphaShow;
 
 public class MainActivity extends Activity {
 
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
+
+    private CharSequence drawerTitle;
+    private CharSequence title;
+    private String[] leftDrawerTitles;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        //登录测试
+        title = drawerTitle = getTitle();
+        leftDrawerTitles = getResources().getStringArray(R.array.left_drawer_array);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList= (ListView) findViewById(R.id.left_drawer);
+
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, leftDrawerTitles));
+        drawerList.setOnItemClickListener(new DrawerItemSelectedListener());
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer,
+                R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                setTitle(drawerTitle);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                setTitle(title);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+
+
+        /*//登录测试
         Button loginButton = (Button) findViewById(R.id.login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,6 +123,94 @@ public class MainActivity extends Activity {
                 intent.putExtra("userName","defaultUser");
                 startActivity(intent);
             }
-        });
+        });*/
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        //menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.title = title;
+        getActionBar().setTitle(title);
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment;
+        FragmentManager fragmentManager = getFragmentManager();
+        switch (position) {
+            case 0:  //若用户已登录，则为「个人中心」，否则为「注册登录」
+                fragment = new PersonalCenter();
+                Bundle args = new Bundle();
+                args.putString("name", "Vincent");
+                fragment.setArguments(args);
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+                break;
+            case 1:
+                break;
+            case 3: // [我的收藏]模块
+                fragment = new UserCollection();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+                break;
+            case 4:
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                break;
+            case 5:
+                Intent intent = new Intent();
+                intent.putExtra("carID", "123");
+                intent.setClass(MainActivity.this, CarShow.class);
+                startActivity(intent);
+                break;
+            default:
+                fragment = new Fragment();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+                break;
+        }
+
+        drawerList.setItemChecked(position, true);
+        setTitle(leftDrawerTitles[position]);
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    private class DrawerItemSelectedListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            selectItem(i);
+        }
     }
 }
--- src/com/Doric/CarBook/member/PersonalCenter.java
+++ src/com/Doric/CarBook/member/PersonalCenter.java
@@ -1,16 +1,18 @@
 package com.Doric.CarBook.member;
 
 
-import android.app.Activity;
+import android.app.Fragment;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 import com.Doric.CarBook.R;
 
-public class PersonalCenter extends Activity implements View.OnClickListener {
+public class PersonalCenter extends Fragment implements View.OnClickListener {
 
     //定义控件
     private Button btnInformation,btnComment, btnFavourite,btnLogOut;
@@ -18,15 +20,21 @@ public class PersonalCenter extends Activity implements View.OnClickListener {
     //定义变量
     private String name;
 
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.personal_center, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.personal_center);
+        View mView = getView();
 
         //设置控件
-        btnFavourite = (Button) findViewById(R.id.name);
-        btnInformation = (Button) findViewById(R.id.head);
-        btnComment = (Button) findViewById(R.id.sex);
-        btnLogOut = (Button) findViewById(R.id.log_out);
+        btnFavourite = (Button) mView.findViewById(R.id.name);
+        btnInformation = (Button) mView.findViewById(R.id.head);
+        btnComment = (Button) mView.findViewById(R.id.sex);
+        btnLogOut = (Button) mView.findViewById(R.id.log_out);
 
         //添加监听器
         btnComment.setOnClickListener(this);
@@ -35,14 +43,14 @@ public class PersonalCenter extends Activity implements View.OnClickListener {
         btnLogOut.setOnClickListener(this);
 
         //设置Actionbar
-        getActionBar().setTitle("我的主页");
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setTitle("我的主页");
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
 
         //取得启动该Activity的Intent对象
-        Intent intent =getIntent();
+        //Intent intent =getIntent();
 
         //取出Intent中附加的数据
-        name = intent.getStringExtra("name");
+        name = getArguments().getString("name");
 
     }
 
@@ -51,7 +59,7 @@ public class PersonalCenter extends Activity implements View.OnClickListener {
 
         //"我的资料"按钮
         if (id == R.id.head) {
-            Intent intent = new Intent(PersonalCenter.this, MyInformation.class);
+            Intent intent = new Intent(getActivity(), MyInformation.class);
             intent.putExtra("name",name);
             startActivity(intent);
         }
@@ -79,7 +87,7 @@ public class PersonalCenter extends Activity implements View.OnClickListener {
     //退出登录对话框
     public void logOutDialog()
     {
-        AlertDialog.Builder builder = new AlertDialog.Builder(PersonalCenter.this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setMessage("确定要退出登录吗？");
         builder.setTitle("退出登录");
         builder.setPositiveButton("取消", new DialogInterface.OnClickListener() {
@@ -90,7 +98,7 @@ public class PersonalCenter extends Activity implements View.OnClickListener {
         builder.setNegativeButton("确认", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
-                PersonalCenter.this.finish();
+                //PersonalCenter.this.finish();
             }
         });
         builder.create().show();
