e43d22a7802a7d3ff98d617aaf09053b4ae2413a
Vincentbel <buaazqh@gmail.com>
2014-06-04 17:00:07 +0800

    修改应用后台网址，可直接利用普通网络访问

--- src/com/Doric/CarBook/Constant.java
+++ src/com/Doric/CarBook/Constant.java
@@ -3,5 +3,5 @@ package com.Doric.CarBook;
 
 public class Constant {
 
-    public static final String BASE_URL = "http://192.168.137.1/doric";
+    public static final String BASE_URL = "http://1.carbook.sinaapp.com/php";
 }
